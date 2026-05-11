76bb591562ae2d86e2673b00294a9bfe1975ab62
sam cocquyt <samcocquyt04@gmail.com>
2026-05-10 22:34:59 -0400

    added read_data function and create_plot function and generated a picture successfully

--- Lab16_xenzonz-1.py
+++ Lab16_xenzonz-1.py
@@ -11,11 +11,57 @@ v. 5/10/2026
 """
 
 import csv
-import datetime
+from datetime import datetime
 import matplotlib.pyplot as plt
 
+def read_data(filename):
+
+    dates = []
+    unemployment_rates = []
+
+    with open(filename, "r", newline="") as file:
+        reader = csv.reader(file)
+
+        header_row = next(reader)
+        for index, column_header in enumerate(header_row):
+            print(index, column_header)
+
+        for row_number, row in enumerate(reader, start=2):
+            try:
+                current_date = datetime.strptime(row[0], "%Y-%m-%d")
+                unemployment_rate = float(row[1])
+            except ValueError:
+                print(f"Skipping row {row_number}: could not convert data.")
+            else:
+                dates.append(current_date)
+                unemployment_rates.append(unemployment_rate)
+
+    return dates, unemployment_rates
+
+
+def create_plot(dates, unemployment_rates):
+    plt.style.use("fivethirtyeight")
+    plt.figure(figsize=(12, 7))
+    plt.plot(dates, unemployment_rates, color="red")
+
+    plt.title("Ohio Unemployment (by Month): 1976 - 2022", fontsize=24)
+    plt.xlabel("Date", fontsize=14)
+    plt.ylabel("Unemp Rate", fontsize=14)
+
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.savefig("ohio_unemployment.png")
+
+
+
+    
+
 def main():
-    pass
+    filename = "OHUR.csv"
+    dates, unemployment_rates = read_data(filename)
+    create_plot(dates, unemployment_rates)
+
 
 if __name__ == "__main__":
     main()
