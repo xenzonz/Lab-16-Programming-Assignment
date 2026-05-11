3a4f39b1bc276f32de3135ea47a8da5d5d49a29c
sam cocquyt <samcocquyt04@gmail.com>
2026-05-10 22:20:42 -0400

    imported csv, datetime, matplotlib, and added main function and added initial docstring

--- Lab16_xenzonz-1.py
+++ Lab16_xenzonz-1.py
@@ -1,1 +1,19 @@
-print("helloworld")
+"""
+Docstring for Lab16_xenzonz-1.py
+i. Lab 15: Plot Ohio Unemployment Data
+ii. Sam Cocquyt
+iii. Read Ohio unemployment data from OHUR.csv, parse the dates and unemployment
+        rates, and create a time-series line plot saved as an image file.
+iv. No starter code
+v. 5/10/2026
+"""
+
+import csv
+import datetime
+import matplotlib.pyplot as plt
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
